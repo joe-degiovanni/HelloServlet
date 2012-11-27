eae46c65a80973393ebec67e8f130a118e92b6a6
Joe Degiovanni <joe.degiovanni@gmail.com>
2012-11-26 22:16:56 -0600

    adding more legit looking servelt code

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,22 @@
-public class Test{
-	public Test(){
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Test extends HttpServlet{
 	
-	}
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
 	
-	public static void main(String[] args){
-		System.out.println("Hello Jenkins");
-	}
 }
